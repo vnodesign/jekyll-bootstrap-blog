b1584b19232e621a86f729cd91d350db253b5371
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2021-07-03 02:51:08 +0700

    Update test

---
+++ assets/js/bundle.js
@@ -0,0 +1,7 @@
+//= require jquery
+//= require turbolinks
+//= require plugins
+//= require bootstrap.bundle
+//= require defer
+'IntersectionObserver' in window||document.write('<link rel="preload" href="/assets/js/polyfill.min.js" as="script" \/><script src="/assets/js/polyfill.min.js"><\/script>');
+Turbolinks.start();
