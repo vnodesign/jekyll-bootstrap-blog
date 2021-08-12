7d29aaea6da37ab2cd4c1ba94be51a196e1392e6
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2021-08-12 22:18:27 +0700

    Update

--- purgecss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    // These are the files that Purgecss will search through
-    content: ["./_site/**/*.html"],
-  
-    // These are the stylesheets that will be subjected to the purge
-    css: ["./_site/dist/main.css"],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-  };
-  
